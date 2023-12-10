aadb842d1d72f0dfd27886b2c71fdb90821563e1
Piero <pirengifor@alumno.unsm.edu.pe>
2023-12-10 14:39:43 -0500

    go

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,16 +1,14 @@
 require("@nomicfoundation/hardhat-toolbox");
-<<<<<<< HEAD
+
 require('dotenv').config()
 
 const account = process.env.DEPLOYER_SIGNER_PRIVATE_KEY;
 const endpoint = process.env.INFURA_ENDPOINT;
-=======
->>>>>>> ef8d48881c0dda3cbd093515e24d2ac1ba9a6d4b
+
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
   solidity: "0.8.19",
-<<<<<<< HEAD
   networks: {
     sepolia: {
       url:endpoint,
@@ -18,7 +16,3 @@ module.exports = {
     }
   }
 };
-
-=======
-};
->>>>>>> ef8d48881c0dda3cbd093515e24d2ac1ba9a6d4b
